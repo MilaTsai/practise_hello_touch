836c34b11a7043c3a087967266fff07bfe35bdce
MilaTsai <emily0912771607@gmail.com>
2017-11-17 22:56:53 +0800

    Initial Commit

---
+++ practise_hello_touch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  practise_hello_touch
+//
+//  Created by 蔡孟軒 on 2017/11/17.
+//  Copyright © 2017年 Mila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("What's up!!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
