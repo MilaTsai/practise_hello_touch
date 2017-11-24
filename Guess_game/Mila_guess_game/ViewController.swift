d0e70ab4b4666c9d55d07f23212df03dd606e826
MilaTsai <emily0912771607@gmail.com>
2017-11-24 23:32:30 +0800

    first commit

---
+++ Guess_game/Mila_guess_game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mila_guess_game
+//
+//  Created by 蔡孟軒 on 2017/11/24.
+//  Copyright © 2017年 Mila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
